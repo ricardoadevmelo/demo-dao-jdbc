867ad827997747cc780c9d77da60adcfca747efd
ricardoadevmelo <121776568+ricardoadevmelo@users.noreply.github.com>
2023-01-09 15:58:51 -0300

    findById implementation

--- src/model/dao/imple/SellerDaoJDBC.java
+++ src/model/dao/imple/SellerDaoJDBC.java
@@ -1,11 +1,25 @@
 package model.dao.imple;
 
+import db.DB;
+import db.DbException;
 import model.dao.SellerDao;
+import model.entities.Department;
 import model.entities.Seller;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
 
 public class SellerDaoJDBC implements SellerDao {
+
+    private Connection connection;
+
+    public SellerDaoJDBC(Connection connection) {
+        this.connection = connection;
+    }
+
     @Override
     public void insert(Seller seller) {
 
@@ -23,7 +37,39 @@ public class SellerDaoJDBC implements SellerDao {
 
     @Override
     public Seller findById(Integer id) {
-        return null;
+        PreparedStatement pSt = null;
+        ResultSet resultSet = null;
+        try {
+            pSt = connection.prepareStatement(
+                    "SELECT seller.*,department.Name as DepName "
+                            + "FROM seller INNER JOIN department "
+                            + "ON seller.DepartmentId = department.Id "
+                            + "WHERE seller.Id = ?");
+
+            pSt.setInt(1, id);
+            resultSet = pSt.executeQuery();
+            if (resultSet.next()) {
+                Department department = new Department();
+                department.setId(resultSet.getInt("DepartmentId"));
+                department.setName(resultSet.getString("DepName"));
+                Seller seller = new Seller();
+                seller.setId(resultSet.getInt("Id"));
+                seller.setName(resultSet.getString("Name"));
+                seller.setEmail(resultSet.getString("Email"));
+                seller.setBaseSalary(resultSet.getDouble("BaseSalary"));
+                seller.setBirthDate(resultSet.getDate("BirthDate"));
+                seller.setDepartment(department);
+                return seller;
+            }
+            return null;
+        }
+        catch (SQLException sqlException){
+            throw new DbException(sqlException.getMessage());
+        }
+        finally {
+            DB.closeStatement(pSt);
+            DB.closeResultSet(resultSet);
+        }
     }
 
     @Override
