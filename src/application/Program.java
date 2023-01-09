71aab8c66924b5d653490bd921aff5740376767e
ricardoadevmelo <121776568+ricardoadevmelo@users.noreply.github.com>
2023-01-09 15:05:09 -0300

    Creating project and git repository

--- src/application/Program.java
+++ src/application/Program.java
@@ -11,27 +11,7 @@ public class Program {
     public static void main(String[] args) {
 
         Connection connection = null;
-        PreparedStatement pSt = null;
 
-        try {
-            connection = DB.getConnection();
-            pSt = connection.prepareStatement(
-                    "DELETE FROM department "
-                            + "WHERE "
-                            + "Id = ?");
-
-            pSt.setInt(1, 5);
-
-            int rowsAffected = pSt.executeUpdate();
-
-            System.out.println("Done! Rows affected: " + rowsAffected);
-        }
-        catch (SQLException sqlException){
-            throw new DbIntegrityException(sqlException.getMessage());
-        }
-        finally {
-            DB.closeStatement(pSt);
-            DB.closeConnection();
-        }
+        DB.closeConnection();
     }
 }
