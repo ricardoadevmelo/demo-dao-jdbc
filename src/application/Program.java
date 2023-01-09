c6b1c2383cd9b9dcb09fdbc0350a7a707133ce14
ricardoadevmelo <121776568+ricardoadevmelo@users.noreply.github.com>
2023-01-09 15:23:38 -0300

    Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -3,16 +3,21 @@ package application;
 import db.DB;
 import db.DbIntegrityException;
 import model.entities.Department;
+import model.entities.Seller;
 
 import java.sql.*;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class Program {
     public static void main(String[] args) {
 
         Department department = new Department(1, "Books");
-        System.out.println(department);
+
+        Seller seller = new Seller(21, "Bob", "bob@gmail.com", new Date(), 3000.0, department);
+
+        System.out.println(seller);
 
     }
 }
